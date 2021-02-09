d34b28451b4de819f77160126f8ab989f5fd4d69
Benjamin Voigt <benjamin.voigt@vapita.de>
2021-02-09 06:09:48 +0100

    moved default controller definition to .env

--- public/index.php
+++ public/index.php
@@ -10,7 +10,6 @@ define('project_root', dirname(__DIR__));
 define('host', $_SERVER['HTTP_HOST']);
 
 $app = new Bootstrap();
-$app->setController('App\Controller\BaseController');
 $app->init();
 
 ob_flush();
